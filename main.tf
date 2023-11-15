bf5a04b12d711a874312d2ae7283b53eda99ace8
Mike Heft <mikeheft@gmail.com>
2023-11-15 06:43:15 -0700

    stuff

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create an AMI that will start a machine whose root device is backed by
+# an EBS volume populated from a snapshot. We assume that such a snapshot
+# already exists with the id "snap-xxxxxxxx".
+resource "aws_ami" "example" {
+  name = "terraform-udemy-course"
+  architecture = var.architecture
+}
+
+resource "aws_instance" "myec2" {
+  ami = aws_ami.example.id
+  instance_type = var.instance_type
+}
+
+resource "aws_eip" "lb" {
+  domain = "vpc"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpn_ip]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+output "public_ip" {
+  value = aws_eip.lb.public_ip
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpn_ip" {
+  default = "116.50.30.20/32"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "architecture" {
+  default = "x86_64"
+}
