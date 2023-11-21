a6304f11d72ebaadf8fc77a5e6457f48dc7f0423
Mike Heft <mikeheft@gmail.com>
2023-11-21 08:07:12 -0700

    Ensure using free tier ami

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,8 @@ Provides an EC2 instance resource.
 This allows instances to be created, updated, and deleted. Instances also support provisioning.
 */
 resource "aws_instance" "myec2" {
-  ami           = data.aws_ami.app_ami.id
+  # ami           = data.aws_ami.app_ami.id
+  ami           = "ami-0230bd60aa48260c6"
   instance_type = var.types["us-west-1"]
   count         = 3
 
