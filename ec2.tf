b0752253d029dac424d56c58c67f4ad3870b7172
Mike Heft <mikeheft@gmail.com>
2023-11-26 06:17:20 -0700

    work on connection and provisioner

--- ec2.tf
+++ ec2.tf
@@ -6,4 +6,24 @@ resource "aws_instance" "myec2" {
   # ami           = data.aws_ami.app_ami.id
   ami           = "ami-0230bd60aa48260c6"
   instance_type = var.types["us-west-1"]
+
+  # Establishes connection to be used by all
+  # generic remote provisioners (i.e. file/remote-exec)
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./terraform-udemy-course.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras install -y nginx1",
+      "systemctl nginx",
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.myec2.private_ip} >> private_ips.txt"
+  }
 }
